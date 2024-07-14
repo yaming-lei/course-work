493904d5033fdcd2b47a12750175b50ef44470d8
yaming-lei <133396429+yaming-lei@users.noreply.github.com>
2024-07-15 01:41:04 +0800

    Add files via upload

---
+++ sign.py
@@ -0,0 +1,18 @@
+import eth_account
+from web3 import Web3
+from eth_account.messages import encode_defunct
+
+def sign(m):
+    w3 = Web3()
+    # create an eth account and recover the address (derived from the public key) and private key
+    account = eth_account.Account.create()
+    address = account.address
+    key = account.privateKey
+
+    # generate signature
+    message = encode_defunct(text=m)
+    signed_message = w3.eth.account.sign_message(message, private_key=key)
+
+    assert isinstance(signed_message, eth_account.datastructures.SignedMessage)
+
+    return address, signed_message
