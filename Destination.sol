6355ae3c58c39b70f46b63d5f828f0fe3f495744
yaming-lei <133396429+yaming-lei@users.noreply.github.com>
2024-07-22 23:08:27 +0800

    Update Destination.sol

--- Destination.sol
+++ Destination.sol
@@ -22,50 +22,41 @@ contract Destination is AccessControl {
         _grantRole(WARDEN_ROLE, admin);
     }
 
-function wrap(address _underlying_token, address _recipient, uint256 _amount ) public onlyRole(WARDEN_ROLE) {
-    address wrapped_token_address = wrapped_tokens[_underlying_token];
-    require(wrapped_token_address != address(0), "Token not registered");  
-    BridgeToken wrappedToken = BridgeToken(wrapped_token_address);
-    wrappedToken.mint(_recipient, _amount);
+function wrap(address _underlying_token, address _recipient, uint256 _amount) public onlyRole(WARDEN_ROLE) {
+        address wrapped_token_address = wrapped_tokens[_underlying_token];
+        require(wrapped_token_address != address(0), "Token not registered");
+        
+        BridgeToken wrappedToken = BridgeToken(wrapped_token_address);
+        wrappedToken.mint(_recipient, _amount);
+        
+        emit Wrap(_underlying_token, wrapped_token_address, _recipient, _amount);
+    }
 
-
-    emit Wrap(_underlying_token, wrapped_token_address, _recipient, _amount);
-}
-
-
-function unwrap(address _wrapped_token, address _recipient, uint256 _amount ) public {
-
+function unwrap(address _wrapped_token, address _recipient, uint256 _amount) public {
     BridgeToken wrappedToken = BridgeToken(_wrapped_token);
     address underlying_token = wrappedToken.underlying();
-
-
+    
     require(wrappedToken.balanceOf(msg.sender) >= _amount, "Insufficient balance");
-
-
-    wrappedToken.burnFrom(msg.sender, _amount);
-
-
+    
+    wrappedToken.burn(_amount); // Use burn instead of burnFrom
+    
     emit Unwrap(underlying_token, _wrapped_token, msg.sender, _recipient, _amount);
 }
 
-
-function createToken(address _underlying_token, string memory name, string memory symbol ) public onlyRole(CREATOR_ROLE) returns(address) {
-
+function createToken(address _underlying_token, string memory name, string memory symbol) public onlyRole(CREATOR_ROLE) returns(address) {
     require(wrapped_tokens[_underlying_token] == address(0), "Token already exists");
-
+    
     BridgeToken wrappedToken = new BridgeToken(_underlying_token, name, symbol, msg.sender);
     address wrapped_token_address = address(wrappedToken);
-
-
+    
     underlying_tokens[_underlying_token] = wrapped_token_address;
     wrapped_tokens[_underlying_token] = wrapped_token_address;
     tokens.push(_underlying_token);
-
+    
     emit Creation(_underlying_token, wrapped_token_address);
-
+    
     return wrapped_token_address;
 }
-
 }
 
 
