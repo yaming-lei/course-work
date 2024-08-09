2fb90f5225c605a38d81202ae49a1c9ec8f7241c
yaming-lei <133396429+yaming-lei@users.noreply.github.com>
2024-08-10 05:03:28 +0800

    Update bridge.py

--- bridge.py
+++ bridge.py
@@ -1,7 +1,7 @@
 from web3 import Web3
 from web3.contract import Contract
 from web3.providers.rpc import HTTPProvider
-from web3.middleware import geth_poa_middleware #Necessary for POA chains
+from web3.middleware import geth_poa_middleware  # Necessary for POA chains
 import json
 import sys
 from pathlib import Path
@@ -12,18 +12,20 @@ contract_info = "contract_info.json"
 private_key = "b3d8146f623407e7691479caeefb6332b60665ad9fee90a8697feaac79b783b0"
 account_address = '0x3b7f6e8011Bc2137ea1Aca77fAeDbe84Af46374A'
 
+
 def connectTo(chain):
     if chain == 'avax':
-        api_url = f"https://api.avax-test.network/ext/bc/C/rpc" #AVAX C-chain testnet
+        api_url = f"https://api.avax-test.network/ext/bc/C/rpc"  # AVAX C-chain testnet
 
     if chain == 'bsc':
-        api_url = f"https://data-seed-prebsc-1-s1.binance.org:8545/" #BSC testnet
+        api_url = f"https://data-seed-prebsc-1-s1.binance.org:8545/"  # BSC testnet
 
-    if chain in ['avax','bsc']:
+    if chain in ['avax', 'bsc']:
         w3 = Web3(Web3.HTTPProvider(api_url))
         # inject the poa compatibility middleware to the innermost layer
         w3.middleware_onion.inject(geth_poa_middleware, layer=0)
     return w3
+
 
 def getContractInfo(chain):
     """
@@ -32,15 +34,16 @@ def getContractInfo(chain):
     """
     p = Path(__file__).with_name(contract_info)
     try:
-        with p.open('r')  as f:
+        with p.open('r') as f:
             contracts = json.load(f)
     except Exception as e:
-        print( "Failed to read contract info" )
-        print( "Please contact your instructor" )
-        print( e )
+        print("Failed to read contract info")
+        print("Please contact your instructor")
+        print(e)
         sys.exit(1)
 
     return contracts[chain]
+
 
 def scanBlocks(chain):
     """
@@ -51,50 +54,60 @@ def scanBlocks(chain):
         When Unwrap events are found on the destination chain, call the 'withdraw' function on the source chain
     """
 
-    if chain not in ['source','destination']:
-        print( f"Invalid chain: {chain}" )
+    if chain not in ['source', 'destination']:
+        print(f"Invalid chain: {chain}")
         return
-    
-    #YOUR CODE HERE
+
+    # YOUR CODE HERE
     w3_src = connectTo(source_chain)
     w3_dst = connectTo(destination_chain)
     source_contracts = getContractInfo("source")
     destination_contracts = getContractInfo("destination")
-    source_contract_address, src_abi = source_contracts["address"], source_contracts["abi"]
-    destination_contract_address, dst_abi = destination_contracts["address"], destination_contracts["abi"]
-    
-    source_contract = w3_src.eth.contract(address=source_contract_address, abi=src_abi)
-    destination_contract = w3_dst.eth.contract(address=destination_contract_address, abi=dst_abi)
+    source_contract_address, src_abi = source_contracts["address"], \
+                                       source_contracts["abi"]
+    destination_contract_address, dst_abi = destination_contracts["address"], \
+                                            destination_contracts["abi"]
 
-    
+    source_contract = w3_src.eth.contract(address=source_contract_address,
+                                          abi=src_abi)
+    destination_contract = w3_dst.eth.contract(
+        address=destination_contract_address, abi=dst_abi)
+
     src_end_block = w3_src.eth.get_block_number()
     src_start_block = src_end_block - 5
     dst_end_block = w3_dst.eth.get_block_number()
     dst_start_block = dst_end_block - 5
 
     arg_filter = {}
-    if chain == "source":  #Source
-        
-        event_filter = source_contract.events.Deposit.create_filter(fromBlock=src_start_block, toBlock = src_end_block, argument_filters=arg_filter)
+    if chain == "source":  # Source
+
+        event_filter = source_contract.events.Deposit.create_filter(
+            fromBlock=src_start_block, toBlock=src_end_block,
+            argument_filters=arg_filter)
         for event in event_filter.get_all_entries():
-            txn = destination_contract.functions.wrap(event.args['token'], event.args['recipient'], event.args['amount']).build_transaction({
-                'from': account_address,
-                'chainId': w3_dst.eth.chain_id,
-                'gas': 5000000,
-                'nonce': w3_dst.eth.get_transaction_count(account_address)
-            })
-            signed_txn = w3_dst.eth.account.sign_transaction(txn, private_key=private_key)
+            txn = source_contract.functions.registerToken(token_address).buildTransaction(
+                {
+                    'from': account_address,
+                    'chainId': w3_dst.eth.chain_id,
+                    'gas': 5000000,
+                    'nonce': w3_dst.eth.get_transaction_count(account_address)
+                })
+            signed_txn = w3_dst.eth.account.sign_transaction(txn,
+                                                             private_key=private_key)
             w3_dst.eth.send_raw_transaction(signed_txn.rawTransaction)
 
-    elif chain == "destination":  #Destination
-        
-        event_filter = destination_contract.events.Unwrap.create_filter(fromBlock=dst_start_block, toBlock = dst_end_block, argument_filters=arg_filter)
+    elif chain == "destination":  # Destination
+
+        event_filter = destination_contract.events.Unwrap.create_filter(
+            fromBlock=dst_start_block, toBlock=dst_end_block,
+            argument_filters=arg_filter)
         for event in event_filter.get_all_entries():
-            txn = source_contract.functions.withdraw(event.args['underlying_token'], event.args['to'], event.args['amount']).build_transaction({
-            'from': account_address,
-            'chainId': w3_src.eth.chain_id,
-            'gas': 500000,
-            'nonce': w3_src.eth.get_transaction_count(account_address)
+            txn = destination_contract.functions.createToken(token_address, token_name, token_symbol).buildTransaction({
+                'from': account_address,
+                'chainId': w3_src.eth.chain_id,
+                'gas': 500000,
+                'nonce': w3_src.eth.get_transaction_count(account_address)
             })
-            signed_txn = w3_src.eth.account.sign_transaction(txn, private_key=private_key)
+            signed_txn = w3_src.eth.account.sign_transaction(txn,
+                                                             private_key=private_key)
             w3_src.eth.send_raw_transaction(signed_txn.rawTransaction)
