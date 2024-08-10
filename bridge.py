20ae852c44aaa847b340ed50e6924a7ae24edd71
yaming-lei <133396429+yaming-lei@users.noreply.github.com>
2024-08-10 23:43:00 +0800

    Update bridge.py

--- bridge.py
+++ bridge.py
@@ -97,7 +97,7 @@ def scanBlocks(chain):
                     ).build_transaction({
                         'from': account_address,
                         'chainId': w3_src.eth.chain_id, 
-                        'gas': 200000,
+                        'gas': 2000000,
                         'nonce': w3_src.eth.get_transaction_count(account_address, 'pending')
                     })
                     signed_txn = w3_src.eth.account.sign_transaction(txn, private_key=private_key)
